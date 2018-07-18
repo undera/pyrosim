e626afdb731fe0cf9ee381161587aca8a40a7c36
Andrey Pohilko <apc4@ya.ru>
2018-07-18 20:41:25 +0300

    Revert more

--- demos/ludobots/Demo_00_Empty_Sim.py
+++ demos/ludobots/Demo_00_Empty_Sim.py
@@ -2,6 +2,6 @@ import sys
 sys.path.insert(0, '../..')
 import pyrosim # noqa
 
-sim = pyrosim.Simulator(use_textures=True, eval_time=1000)
+sim = pyrosim.Simulator()
 sim.start()
 sim.wait_to_finish()
